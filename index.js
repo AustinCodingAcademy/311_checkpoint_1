f6749fdc02dbad23939783add94d2af3824ecadc
grantkee <grant.d.kee@gmail.com>
2019-09-21 10:32:31 -0500

    complete - grantk

--- index.js
+++ index.js
@@ -3,7 +3,11 @@ const app = express()
 const bodyParser = require('body-parser')
 const port = process.env.PORT || 4000
 
+let routerFiles = require('./routes/users')
+
 app.use(bodyParser.json())
+
+app.use(routerFiles)
 
 app.get('/', (req, res) => res.send('default route'))
 
