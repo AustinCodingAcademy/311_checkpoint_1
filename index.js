8f5fb6a836177c73c3daa4fd0d9b8d1b8b5dd895
fagner henrique flores <henriquefagner@hotmail.com>
2019-09-24 11:54:38 -0500

    final commit Documents/311_checkpoint_1

--- index.js
+++ index.js
@@ -1,9 +1,16 @@
-const express = require('express')
-const app = express()
-const port = process.env.PORT || 4000
+const express = require('express');
+const app = express();
+const bodyparsen = require('body-parser');
+const port = process.env.PORT || 4000;
 
-app.get('/', (req, res) => res.send('default route'))
+const routerFiles = require('./routes/users');
+
+app.use(bodyparsen.json());
+
+app.use(routerFiles);
+
+app.get('/', (req, res) => res.send('default route'));
 
 app.listen(port, () => {
-  console.log('app is listening on:', port)
-})
+  console.log('app is listening on:', port);
+});
