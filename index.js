64b980b395cd6e013769bcefebbdc785935c1757
John Obertubbesing <oberthewhat@gmail.com>
2019-11-13 17:05:33 -0600

    checkpoint updated

--- index.js
+++ index.js
@@ -1,9 +1,12 @@
 const express = require('express')
 const app = express()
 const port = process.env.PORT || 4000
+const bodyParser = require("body-parser");
+const userRoutes = require("./routes/users")
 
-app.get('/', (req, res) => res.send('default route'))
-
+app.use( bodyParser.json() );
+app.get('/', (req, res) => res.send('Get a Beer'))
+app.use(userRoutes)
 app.listen(port, () => {
-  console.log('app is listening on:', port)
+  console.log(`app is listening on: ${port}`)
 })
