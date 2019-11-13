3f6f4c846df974a69cdf4519001bd32edbefb7b5
kevinjj30 <kevinjenkins56@yahoo.com>
2019-11-12 20:51:23 -0600

    finished

--- index.js
+++ index.js
@@ -1,6 +1,13 @@
 const express = require('express')
 const app = express()
+const bodyParser = require('body-parser')
 const port = process.env.PORT || 4000
+
+let routerFiles = require('./routes/users')
+
+app.use(bodyParser.json())
+
+app.use(routerFiles)
 
 app.get('/', (req, res) => res.send('default route'))
 
