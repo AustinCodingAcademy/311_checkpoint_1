c71ca4e692609737f03a95782a1919ea70823cad
vincentnqc <vincentnqc92@gmail.com>
2019-11-13 17:09:09 -0600

    cleaned up

--- index.js
+++ index.js
@@ -2,9 +2,9 @@ const express = require('express')
 const app = express()
 const port = process.env.PORT || 4000
 const bodyParser = require("body-parser")
-app.use(bodyParser.json())
 const userRoutes = require("./routes/users")
 
+app.use(bodyParser.json())
 app.use(userRoutes)
 
 app.get('/', (req, res) => res.send('default route'))
