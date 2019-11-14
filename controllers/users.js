cf0b9a379f9d47f57e0709c3cde01892e80c6412
Alex Munoz <anmunoz24@gmail.com>
2019-11-14 08:09:53 -0800

    status codes

--- controllers/users.js
+++ controllers/users.js
@@ -10,13 +10,15 @@ const get = (req, res) => {
 
 const getId = (req, res) => {
   let usersId = parseInt(req.params.userId)
-  console.log(req.params.userId)
+  if (usersId) {
   let usersIndex = parseInt(req.params.userId) - 1;
-  
   for (let i = 0; i < users.length; i++) {
     if (i + 1 === usersId)  {
       res.json(users[usersIndex])
-    }   
+      }   
+    }
+  }  else {
+    res.status(400).json({msg: `User ${req.params.id} not found`})
   }
 }
 
@@ -37,24 +39,37 @@ const post = (req, res) => {
 
 
 const put = (req, res) => {
-  const foundIndex = users.findIndex(x => x.id == (req.params.userId));
-  users[foundIndex].name = req.body.name;
-  users[foundIndex].username = req.body.username;
-  users[foundIndex].email = req.body.email;
-  users[foundIndex].address = req.body.address;
-
-  res.json(users);
+  let person = users.some(user => user.id === parseInt(req.params.id))
+  if (person) {
+    let updated = req.body;
+    users.forEach(user => {
+      if (user.id === parseInt(req.params.id)) {
+        user.name = updated.name ? updated.name : user.name
+      }
+    });
+    // users[foundIndex].name = req.body.name;
+    // users[foundIndex].username = req.body.username;
+    // users[foundIndex].email = req.body.email;
+    // users[foundIndex].address = req.body.address;
+    res.json(users.filter(user => user.id === parseInt(req.params.id)));
+  } else {
+    res.status(400).json({msg: `User ${req.params.id} not found`})
+  }
   };
 
 
 
 const deleteUser = (req, res) => {
-  const foundIndex = users.findIndex(x => x.id == (req.params.userId));
- 
-  users.splice(foundIndex, 1)
- 
-  res.json(users)
-  res.send('User Deleted')
+  const foundIndex = users.findIndex(x => x.id === parseInt(req.params.id));
+
+  if (foundIndex) {
+    res.json({
+      msg: `User has been deleted`, 
+      users: users.splice(foundIndex, 1)
+    })
+  } else {
+    res.status(400).json({msg: `User ${req.params.id} not found`})
+  }
 }
 
 module.exports = { get, getId, post, put, deleteUser }
