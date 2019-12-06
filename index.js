777a2df14f8ca80afa8b75c14ddf0c9dd10fe4e8
TripKings <antonio.cicchese@gmail.com>
2019-12-06 00:03:09 -0600

    finished checkpoint

--- index.js
+++ index.js
@@ -1,10 +1,12 @@
 const express = require('express')
 const app = express()
 const bodyParser = require('body-parser')
-const port = process.env.PORT || 4000
+const port = process.env.PORT || 4002
 const routerFiles = require('./routes/users')
 
-
+app.use(bodyParser.json());
+app.use(express.static("public"))
+app.use(routerFiles)
 
 app.get('/', (req, res) => res.send('default route'))
 
