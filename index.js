a641fa8cdeb40b0467bbea1ecbf0287c2ff3faa6
EmiKusaba <eeemi615@gmail.com>
2020-01-24 11:47:20 -0600

    added file

--- index.js
+++ index.js
@@ -1,6 +1,16 @@
 const express = require('express')
 const app = express()
 const port = process.env.PORT || 4000
+const bodyParser = require('body-parser')
+app.use(bodyParser.json())
+
+
+//routes
+const usersRoute = require("./routes/users") 
+
+//app.use
+
+app.use('/users',usersRoute)
 
 app.get('/', (req, res) => res.send('default route'))
 
