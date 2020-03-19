a30f137fa63aa92b6c3d3ebe3dbecbd1ab836eb9
carl-larson <clarson08@gmail.com>
2020-03-19 13:59:23 -0500

    Got something working for the error catching

--- controllers/users.js
+++ controllers/users.js
@@ -6,12 +6,13 @@ const getAllUsers = (req, res) => {
     res.json(users);
 }
 
-const getUserById = (req, res) => {
+const getUserById = (req, res, next) => {
     const userId = req.params.id;
     res.json(users.find(user => user.id === Number(userId)))
+    return next(new Error('400 User does not exist'))
 }
 
-const addUser = (req, res) => {
+const addUser = (req, res, next) => {
     const userId = users.length + 1;
     let user = {
         id: userId,
@@ -19,9 +20,10 @@ const addUser = (req, res) => {
         }
     users.push(user);
     res.json(users[users.length - 1])
+    return next(new Error('Something went wrong!'))
 }
 
-const updateUser = (req, res) => {
+const updateUser = (req, res, next) => {
     const userId = req.params.id;
     // const changeInfo = req.body;
     const found = users.find(user => user.id === Number(userId));
@@ -30,14 +32,18 @@ const updateUser = (req, res) => {
         ...req.body
     }
     res.json(users[Number(userId)-1]);
+    return next(new Error('400 User does not exist'))
 }
 
-const deleteUserbyId = (req, res) => {
+const deleteUserbyId = (req, res, next) => {
     let userId = req.params.id;
     users.splice(userId - 1, 1);
     console.log(`Deleting ${userId}`);
     res.send(users)
+    return next(new Error('400 User does not exist'))
 }
+
+
 
 module.exports = {
     defaultRoute, getAllUsers, getUserById, addUser, updateUser, deleteUserbyId
--- index.js
+++ index.js
@@ -9,9 +9,16 @@ app.use(bodyParser.json())
 app.use('/', usersRouter)
 
 app.use(function (req, res, next) {
+  // console.log(res.status());
   res.status(404).send("Error 404. Sorry can't find that!");
-  res.status(400).send("Error 400. User does not exist!");
+  // res.status(400).send("Error 400. User does not exist!");
 })
+
+// app.use(function(err, req, res, next) {
+//   res.status(500);
+//   res.send("Oops, something went wrong.")
+// });
+
 
 app.listen(port, () => {
   console.log('app is listening on:', port)
