b5299b08df19e481cb25e943c245b12a37e747bf
hbrashid <hbrashid@gmail.com>
2020-04-01 20:28:40 -0500

    some tweaks

--- index.js
+++ index.js
@@ -1,14 +1,17 @@
 const express = require('express');
+const bodyParser = require('body-parser');
 const users = require('./routes/users');
 const app = express();
-const bodyParser = require('body-parser');
 
+// app.use('/', express.static('public'));
 app.use(bodyParser.json());
+
 app.use(users);
 
 app.get('/', (req, res) => res.send('default route'))
 
-const port = process.env.PORT || 4000
+const port = process.env.PORT || 4000;
+
 app.listen(port, () => {
   console.log('app is listening on:', port)
 })
