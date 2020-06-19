82021091fcbb75e325bc0701a901a2de2f2cbb38
CodingGiraffe <adamthomas03@gmail.com>
2020-06-19 10:39:06 -0500

    complete

---
+++ Routes/users.js
@@ -0,0 +1,16 @@
+const express = require('express')
+const router = express.Router()
+const userController = require('../Controllers/controllers')
+
+
+router.get('/users', userController.listUsers)
+
+router.get('/users/:id', userController.showUser)
+
+router.post('/users', userController.createUser)
+
+router.put('/users/:id', userController.updateUser)
+
+router.delete('/users/:id', userController.deleteUser)
+
+module.exports = router
