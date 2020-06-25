aad8f6db353310fd440e36188ecde1a08232297b
peter gilkey <peter_gilkey@yahoo.com>
2020-06-25 04:54:19 -0500

    Checkpoint finished

--- index.js
+++ index.js
@@ -1,9 +1,16 @@
 const express = require('express')
+const bodyParser = require('body-parser');
 const app = express()
+const users = require('./routes/users');
 const port = process.env.PORT || 4000
 
-app.get('/', (req, res) => res.send('default route'))
+app.use(bodyParser.json());
+app.use(users);
 
-app.listen(port, () => {
+
+app.listen(port, (err) => {
+  if (err) {
+    console.log("Error: ",err);
+  }
   console.log('app is listening on:', port)
 })
---
+++ routes/users.js
@@ -0,0 +1,16 @@
+const express = require('express');
+const router = express.Router();
+const controller = require('../controllers/users');
+
+
+router.get('/users/', controller.listUsers);
+
+router.get('/users/:id', controller.showUser);
+
+router.post('/users/', controller.createUser);
+
+router.put('/users/:id', controller.updateUser);
+
+router.delete('/users/:id', controller.deleteUser);
+
+module.exports = router;
