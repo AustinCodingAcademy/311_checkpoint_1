2ca6c0352edaef2b337eafd82e5fd2969ac98610
@willmcclure <willbmcclure@gmail.com>
2020-12-16 20:58:02 -0600

    50 of 100 pts completed

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const express = require('express')
-const bodyParser = require("body-parser")
+const bodyParser = require('body-parser')
+const usersRouter = require('./routes/users')
 const app = express()
 const port = process.env.PORT || 4000
 
@@ -8,3 +9,6 @@ app.get('/', (req, res) => res.send('default route'))
 app.listen(port, () => {
   console.log('app is listening on:', port)
 })
+
+app.use(bodyParser.json())
+app.use(usersRouter)
