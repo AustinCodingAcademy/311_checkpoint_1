0e93249efa2f1752487c7164f5609b4225cf2eb5
James Messina <jmessina17@gmail.com>
2021-05-06 19:28:02 -0500

    fixing bug for poert number

--- index.js
+++ index.js
@@ -7,6 +7,7 @@ const superHeroesRouter = require('./routes/superHeroes.js');
 const usersRouter = require('./routes/users.js'); 
 const customersRouter = require('./routes/customers.js');
 
+
 //** middleware */
 app.use(bodyParser.json()); 
 app.use(superHeroesRouter);
@@ -14,7 +15,13 @@ app.use(usersRouter);
 app.use(customersRouter); 
 
 
+
+//functions
+
+
+
 //create listen request here 
 app.listen(port, () => {
   console.log('app is listening on port:', port); 
 })
+
