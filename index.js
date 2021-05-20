ba1f138d28ecb204307b5480f0463d127aa668bc
RacMcT <raccmct@gmail.com>
2021-05-20 10:55:42 -0500

    setup commit

--- index.js
+++ index.js
@@ -1,5 +1,8 @@
+
 const express = require('express')
 const app = express()
+const bodyParser = require("body-parser");
+app.use(bodyParser.json());
 const port = process.env.PORT || 4000
 
 app.get('/', (req, res) => res.send('default route'))
@@ -7,3 +10,4 @@ app.get('/', (req, res) => res.send('default route'))
 app.listen(port, () => {
   console.log('app is listening on:', port)
 })
+
