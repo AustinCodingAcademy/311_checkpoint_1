c6bf060b82704a61683ae9cd04ae4050e242e019
Dominik Brame <idominikbrame@gmail.com>
2021-05-30 17:22:48 -0500

    complete assigment

---
+++ controllers/user-controller.js
@@ -0,0 +1,61 @@
+const users = require("../data/index")
+const sample = require("../data/sampleUser")
+
+
+function getAllUsers(req, res) {
+  res.json(users)
+}
+
+
+const getUserById = (req, res) => {
+  if (req.params.id > users.length) {
+    res.sendStatus(404)
+  }
+  users.forEach(user => {
+    if (user.id == req.params.id) {
+      res.json(user)
+    }
+  })
+}
+
+const createUser = (req, res) => {
+  let counter = users.length
+  users.push({
+    id: ++counter,
+    name: req.body.name,
+    username: req.body.username,
+    email: req.body.email,
+    address: req.body.address,
+    phone: req.body.phone,
+    website: req.body.website,
+    company: req.body.company
+  })
+  res.json(users[users.length - 1])
+}
+
+const updateUser = (req, res) => {
+  if (req.params.id > users.length) {
+    res.sendStatus(404)
+  }
+  users.forEach(user => {
+    if (user.id == req.params.id) {
+      let index = users.indexOf(user)
+      users[index] = sample
+      res.json(users)
+    }
+  })
+}
+
+const deleteUser = (req, res) => {
+  if (req.params.id > users.length) {
+    res.sendStatus(404)
+  }
+  users.forEach(user => {
+    if (user.id == req.params.id) {
+      let index = users.indexOf(user)
+      users.splice(index, 1)
+      res.json(users)
+    }
+  })
+}
+module.exports = {getAllUsers, getUserById, createUser, deleteUser, updateUser}
--- index.js
+++ index.js
@@ -1,8 +1,10 @@
 const express = require('express')
 const app = express()
 const port = process.env.PORT || 4000
+const router = require('./routes/user-routes')
 
-app.get('/', (req, res) => res.send('default route'))
+
+app.use(router)
 
 app.listen(port, () => {
   console.log('app is listening on:', port)
