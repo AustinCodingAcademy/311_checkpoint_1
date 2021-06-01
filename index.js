43e20d130857e15c6b93751c8c77d2b6e415e328
RacMcT <raccmct@gmail.com>
2021-06-01 12:39:27 -0500

    fixed routes and added controllers

--- index.js
+++ index.js
@@ -1,20 +1,12 @@
-
-const express = require('express')
+const express = require("express")
 const app = express()
-const bodyParser = require("body-parser");
-app.use(bodyParser.json());
+const bodyParser = require("body-parser")
 const port = process.env.PORT || 4000
+const users = require("./routes/users")
 
+app.use(bodyParser.json());
 app.get('/', (req, res) => res.send('default route'))
-
-// routers
-// app.use('/users', require('./routes/users'));
-// app.use('/users', require('./routes/users'));
-// app.use('/users', require('./routes/comments'));
-// app.use('/users', require('./routes/products'));
-
-
+app.use(users)
 app.listen(port, () => {
   console.log('app is listening on:', port)
 })
-
