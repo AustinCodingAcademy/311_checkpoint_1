f4359884f9b50682e6fd53e6c0d4c9e0c69dd611
Vic <vicjs612@gmail.com>
2023-01-24 20:12:06 -0600

    checkpoint 1 complete

--- index.js
+++ index.js
@@ -1,9 +1,16 @@
-const express = require('express')
-const app = express()
-const port = process.env.PORT || 4000
+const express = require('express');
+const app = express();
+const bodyParser = require('body-parser');
+const users = require('./data/index');
+const routes = require('./routes/users');
 
+app.use(bodyParser.json());
+app.use(routes)
 app.get('/', (req, res) => res.send('default route'))
+
+const port = process.env.PORT || 4000;
 
 app.listen(port, () => {
   console.log('app is listening on:', port)
 })
+
